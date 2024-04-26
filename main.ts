c07e0b92ffbdd4e3f324ba2aef0086a9cc5713ec
fourbacon <156138058+fourbacon@users.noreply.github.com>
2024-04-26 09:22:07 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,31 +2,13 @@ namespace SpriteKind {
     export const camera = SpriteKind.create()
     export const Player2 = SpriteKind.create()
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player2, function (player2, wall) {
-    if (player2.vx != 0) {
-        xStoppedAt = wall.x - 16 * Math.abs(player2.vx) / player2.vx
-        yStoppedAt = null
-player2.vx = 0
-        player2.x = xStoppedAt
-    }
-    if (player2.vy != 0) {
-        yStoppedAt = wall.y - 16 * Math.abs(player2.vy) / player2.vy
-        player2.vy = 0
-        player2.y = yStoppedAt
-        xStoppedAt = null
-    }
-    if (player2.x + 16 > wall.x && player2.x < wall.x + 16 || player2.y + 16 > wall.y && player2.y < wall.y + 16) {
-        if (xStoppedAt != null) {
-            player2.x = xStoppedAt
-        } else if (yStoppedAt != null) {
-            player2.y = yStoppedAt
-        }
-    }
-})
 controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     if (mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).tileKindAt(TileDirection.Bottom, sprites.dungeon.floorLight0)) {
         mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).vy = -125
     }
+})
+spriteutils.createRenderable(0, function (screen2) {
+    screen2.drawLine(mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).x, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).y, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).x, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).y, 10)
 })
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     if (mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).tileKindAt(TileDirection.Bottom, sprites.dungeon.floorLight0)) {
@@ -93,8 +75,6 @@ mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).ay = 300
 mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).ay = 300
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 100, 0)
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), 100, 0)
-let xStoppedAt: number
-let yStoppedAt: number
 mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setStayInScreen(true)
 mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).setStayInScreen(true)
 game.onUpdate(function () {
